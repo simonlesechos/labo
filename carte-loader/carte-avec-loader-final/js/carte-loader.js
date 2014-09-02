e5d786d542708a1dc2cc2f5e924e2e52fbcaaa0f
simonlesechos <spatrat@lesechos.fr>
2014-09-02 15:06:50 +0200

    Update de la carte loader ajout d'un selecteur de background

--- carte-loader/carte-avec-loader-final/js/carte-loader.js
+++ carte-loader/carte-avec-loader-final/js/carte-loader.js
@@ -33,36 +33,44 @@ function animateUpdate() {
 
         $('.spinner-bars').animate({opacity: 0});
         tl.staggerFrom($('.y2015'), 0.2, {scale: 0, opacity: 0, ease: Bounce.easeOut})
-        .staggerTo($('.y2015'), 0.7, {scale: 1, opacity: 1, ease: Bounce.easeOut});
+        .staggerTo($('.y2015'), 0.7, {scale: 1, opacity: 1, ease: Bounce.easeOut},0.2);
         
 
-        tl.to($('.maj'), 0.2, {opacity:0});
-
-        tl.to($('.waitcomplete'), 1.5, {scale: 1, opacity:1, ease: Bounce.easeOut},3.5);
-        tl.to($('.waitcomplete'), 0.5, {scale: 4, opacity: 0, ease:Quad.easeOut},5);
-
-        tl.to($('.y2015'), 1.2,{scale:7, opacity: 0, ease:Power2.easeOut},4.9)
+        tl.to($('.maj'), 0.5, {top:"67%"});
+        tl.to($('.wait'), 0.7, {opacity:0, ease: Power2.easeOut},0.5);
+        tl.to($('.waitcomplete'), 1.5, {scale: 1, opacity:1, ease: Bounce.easeOut},1);
+        tl.to($('.waitcomplete'), 0.5, {scale: 4, opacity: 0, ease:Quad.easeOut},3);
+        tl.to($('.maj'),0.7,{opacity:0, ease: Strong.easeOut},3);
+        tl.to($('.y2015'), 1.2,{scale:7, opacity: 0, ease:Power2.easeOut},3.4);
 
         //Face 2 :
-        tl.to($('.couleur-bas'), 0.7, {bottom:0, ease: Strong.easeOut},7);
-        tl.to($('.perso-cabinet'), 0.7, {top: 0, ease: Strong.easeOut},7.2);
-
-        //Face 3 :
-        tl.to($('.carte-back'), 0.7, {top:0, ease: Quad.easeOut},12);
-        tl.staggerFrom($('.fdf'), 0.5, {scale: 0, opacity: 0, rotation: -90, ease:Quad.easeOut, delay:0.5}) 
-        .staggerTo($('.fdf'), 0.5, { opacity: 1, scale:1, rotation : 0}),
-
-        tl.staggerFrom($('.fdf-desc'), 0.4, {scale: 0, opacity: 0, ease: Quad.easeOut}) 
-        .staggerTo($('.fdf-desc'), 0.4, { opacity: 1, scale:1, ease: Quad.easeOut});
-
-
-
-}
-
+        tl.to($('.couleur-bas'), 0.7, {bottom:0, ease: Strong.easeOut},4.5);
+        tl.to($('.perso-cabinet'), 0.7, {top: 0, ease: Strong.easeOut},4.6);
+        tl.to($('.y2015-bas'), 0.7,{right: "10px", opacity: 1, ease:Power2.easeOut},4.7);
         
 
+        //Face 3 :
+        tl.to($('.carte-back'), 0.5, {top:0, ease: Quad.easeOut},10);
+        tl.staggerFrom($('.fdf'), 0.2, {scale: 0, opacity: 0, rotation: -90, ease:Quad.easeOut}) 
+        .staggerTo($('.fdf'), 0.2, { opacity: 1, scale:1, rotation : 0});
+
+        tl.staggerFrom($('.fdf-desc'), 0.4, {scale: 1, opacity: 0, ease: Quad.easeOut}) 
+        .staggerTo($('.fdf-desc'), 0.4, { opacity: 1, scale:1, ease: Quad.easeOut});
 
 }
+  
+}
+
+
+$('.colorspot').click(function(){
+  var bgimage = $(this).data('bg');
+  var bgnew = 'url(' + bgimage + ') repeat';
+  console.log(bgimage);
+  console.log(bgnew);
+  $('.carte').css('background', bgnew);
+  $('.carte-back').css('background', bgnew);
+  $('.couleur-bas').css('background', bgnew);
+});
 
 
 
