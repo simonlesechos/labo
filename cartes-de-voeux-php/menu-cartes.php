e3c43d6848491e4d94932f8a6e1fc2b366a3a7e9
simonlesechos <spatrat@lesechos.fr>
2014-09-08 16:13:23 +0200

    Update avec menu en vignettes

--- cartes-de-voeux-php/menu-cartes.php
+++ cartes-de-voeux-php/menu-cartes.php
@@ -7,14 +7,13 @@
     <div class="root-menu-container">
 
         <h1>Cartes de voeux</h1>
-        <h2 class="big-title">Choisir une carte</h2>
-        <a class="carte-link" href="../carte-2015-etoile/index.php">Carte étoiles</a>
-        <a class="carte-link" href="../carte-2015-typo/index.php">Carte Typo</a>
-        <a class="carte-link" href="../carte-2015-loading/index.php">Carte Loader</a>
-        <a class="carte-link" href="../carte-2015-avion/index.php">Carte Avion</a>
-        <a class="carte-link" href="../carte-2015-chiffres/index.php">Carte Chiffres</a>
-        <a class="carte-link" href="../carte-2015-Template-vide/index.php">Carte Template (vide)</a>
-        <a class="carte-link" href="../toutes-les-cartes/toutes-les-cartes.php">Toutes les cartes</a>
+        <!--<h2 class="big-title">Choisir une carte</h2>-->
+        <a class="carte-link" href="../carte-2015-etoile/index.php"><img src="../ressources/img/vignettes/etoile.jpg"><span>Carte étoiles</span></a>
+        <a class="carte-link" href="../carte-2015-typo/index.php"><img src="../ressources/img/vignettes/typo.jpg"><span>Carte Typo</span></a>
+        <a class="carte-link" href="../carte-2015-loading/index.php"><img src="../ressources/img/vignettes/telechargement.jpg"><span>Carte Loader</span></a>
+        <a class="carte-link" href="../carte-2015-avion/index.php"><img src="../ressources/img/vignettes/aviateur.jpg"><span>Carte Avion</span></a>
+        <a class="carte-link" href="../carte-2015-chiffres/index.php"><img src="../ressources/img/vignettes/chiffres.jpg"><span>Carte Chiffres</span></a>
+        <a class="carte-link" href="../toutes-les-cartes/toutes-les-cartes.php"><img src="../ressources/img/vignettes/all.png"><span>Toutes les cartes</span></a>
 
     </div>
 
