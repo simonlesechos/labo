12b4c4f0dae2d471a9c5565c571d3d9a170f59d7
simonlesechos <spatrat@lesechos.fr>
2014-09-11 11:20:48 +0200

    maj + maquette expert

--- maquette-expert/js/expert.js
+++ maquette-expert/js/expert.js
@@ -1,4 +1,7 @@
 $('.deployerlink').click(function(){
+	$(this).toggleClass('active');
+	$('.deployerlink').not(this).removeClass('active');
 	$(this).next('.large-menu-container').toggleClass('showed');
 	$('.large-menu-container').not($(this).next()).removeClass('showed');
 });
+
